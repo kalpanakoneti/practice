4ddca70af48e78a2d128ef823978f941a6f1274d
Kalpana <Kalpana@192.168.0.100>
2018-06-25 13:39:43 +0530

    demo program

---
+++ src/b/Book.java
@@ -0,0 +1,17 @@
+package b;
+
+public class Book 
+{
+	int id;
+	String name,author,publisher;
+	int quantity;
+	Book(int i,String n,String a,String p,int q)
+	{
+		id=i;
+		name=n;
+		author=a;
+		publisher=p;
+		quantity=q;
+	}
+
+}
